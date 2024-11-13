f80cd136d73e9e8093862f499935364ed1530856
TobiOl <1712264@brunel.ac.uk>
2024-11-13 20:15:55 +0000

    Initial Commit

---
+++ src/main/java/com/nwbproj/primes/PrimesprojectApplication.java
@@ -0,0 +1,13 @@
+package com.nwbproj.primes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimesprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimesprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nwbproj/primes/PrimesprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nwbproj.primes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimesprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
